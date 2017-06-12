ca1c03dbcbe4e90e23a1d6aa7124f16e7e13b6ef
arkadi <arkadi81@gmail.com>
2017-06-12 00:57:58 -0800

    add lessons 1-2, basics and object types

--- README.md
+++ README.md
@@ -1,5 +1,29 @@
 # ruby-tutorial
 Learning basics of ruby
 
-##  interpretted rather than compiled
-## fully object oriented
+- interpretted rather than compiled
+-  fully object oriented
+- whitespace independent
+- no semicolons
+
+usage
+- cli single command
+- from file
+- irb
+
+puts - prints to terminal / screen automatically adds return
+print - doesnt add line return
+
+- name files with .rb extension
+
+- comments: # comments go after pound sign
+
+
+## IRB / irb --simple-prompt
+nil = success run code
+
+- everything is a method. strings too. eg "hello".reverse
+
+- ri (ruby information!) works from cli
+
+technically, variables are not objects, but references to obj.
---
+++ lesson1/hello.rb
@@ -0,0 +1,31 @@
+puts 'hello world'
+# this is a comment
+
+#algebra and assignment 
+x = 1
+puts x+2
+
+#naming convension - all lowercase + underscore, NOT CAMEL
+
+#scope indicators:
+# globals $var
+# Class @@var
+# instance @var
+# local var
+# Block var
+
+# x +=2 works
+# var.class shows which class var belongs to
+# Integer -> Bignum or Fixnum (differnce in size. ruby will take care of correct assignemnt
+# and change as needed)
+
+# -200.abs absolute value.
+# 200.next = 201
+# Floats: (decimals, as std) 10.class = Fixnum, 10.0.class = Float
+# int / int will lose precision, acts like div. for precise calculations, use floats!
+# 123.345.round
+# 123.322.to_i will convert to integer
+# .floor rounds down
+# .ceil (cieling rounds up)
+
+# stopped on vid 02.05 D:\torrents\Lynda.com - Ruby Essential Training
